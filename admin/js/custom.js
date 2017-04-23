6bf2f412c933826c8c67f6d18e9ef0557278ed70
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-04-23 14:16:55 +0400

    Added new method to initialize color picker in plugin settings

--- admin/js/custom.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $(function() {
-		$( '.woo-store-vacation-datepicker' ).datepicker({
-                dateFormat : 'dd-mm-yy',
-                minDate: 1,
-        });
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
