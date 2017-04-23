6ec19ca96b99154c64b0cef16bf39dfd793a533c
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-04-23 14:17:58 +0400

    Updated registered option name

--- uninstall.php
+++ uninstall.php
@@ -9,7 +9,7 @@
 if (!defined( 'WP_UNINSTALL_PLUGIN')):
     exit();
 endif;
-$option_name = 'woo_store_vacation_option_name';
+$option_name = 'woo_store_vacation_options';
 delete_option($option_name);
 // For site options in Multisite
 delete_site_option($option_name);
