76ac037a7bcf828dca6f8d41eaa5ffe9fb4ecfa3
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-12-14 16:49:51 +0300

    Code tidy

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,20 @@
 <?php
 /**
  * Unistall Woo Store Vacation.
+ * Fired when the plugin is uninstalled.
  *
  * @author      Mahdi Yazdani
  * @package     Woo Store Vacation
- * @since       1.0
+ * @since       1.3.0
  */
-if (!defined( 'WP_UNINSTALL_PLUGIN')):
-    exit();
-endif;
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
 $option_name = 'woo_store_vacation_options';
-delete_option($option_name);
+
+delete_option( $option_name );
 // For site options in Multisite
-delete_site_option($option_name);
+delete_site_option( $option_name );
