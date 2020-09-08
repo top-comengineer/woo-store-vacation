edb64f2864c540ea2bf856b6231d17cc80245b78
top-comengineer <yunlong870924@gmail.com>
2020-09-08 14:39:54 +0300

    Removed minimum date from the datepicker module

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,61 @@
 /**
  * Initialize jQuery datepicker, colorpicker
  *
- * @since       1.3.0
- * @package     Woo Store Vacation
+ * @since       1.3.9
+ * @package
  */
-( function( wp, $ ) {
-        'use strict';
+( function ( wp, $ ) {
+	'use strict';
 
-        if ( ! wp ) {
-            return;
-        } // End If Statement
+	if ( ! wp ) {
+		return;
+	} // End If Statement
 
-        var getDate = new Date(),
-            targetTimeOffset = 60,
-            startDate = $( '.woo-store-vacation-start-datepicker' ),
-            endDate = $( '.woo-store-vacation-end-datepicker' );
-        
-        getDate.setMinutes( getDate.getMinutes() + getDate.getTimezoneOffset() + targetTimeOffset );
+	const $startDate = $( '.woo-store-vacation-start-datepicker' ),
+		$endDate = $( '.woo-store-vacation-end-datepicker' );
 
-        // In order to work, we have to call this before any call to datepicker.
-        cleanDatepicker();
+	// In order to work, we have to call this before any call to datepicker.
+	cleanDatepicker();
 
-        startDate.datepicker( {
-            minDate: getDate,
-            changeMonth: true,
-            showButtonPanel: true,
-            dateFormat: 'yy-mm-dd',
-            onClose: function( selectedDate, inst ) {
-                var minDate = new Date( Date.parse( selectedDate ) );
-                minDate.setDate( minDate.getDate() + 1 );
-                endDate.datepicker( 'option', 'minDate', minDate );
-            }
-        } );
+	$startDate.datepicker( {
+		changeMonth: true,
+		showButtonPanel: true,
+		dateFormat: 'yy-mm-dd',
+		onClose( selectedDate ) {
+			const minDate = new Date( Date.parse( selectedDate ) );
+			minDate.setDate( minDate.getDate() + 1 );
+			$endDate.datepicker( 'option', 'minDate', minDate );
+		},
+	} );
 
-        endDate.datepicker( {
-            minDate: '+1D',
-            changeMonth: true,
-            showButtonPanel: true,
-            dateFormat: 'yy-mm-dd',
-            onClose: function( selectedDate, inst ) {
-                var maxDate = new Date( Date.parse( selectedDate ) );
-                maxDate.setDate( maxDate.getDate() - 1 );            
-                startDate.datepicker( 'option', 'maxDate', maxDate );
-            }
-        } );
+	$endDate.datepicker( {
+		minDate: '+1D',
+		changeMonth: true,
+		showButtonPanel: true,
+		dateFormat: 'yy-mm-dd',
+		onClose( selectedDate ) {
+			const maxDate = new Date( Date.parse( selectedDate ) );
+			maxDate.setDate( maxDate.getDate() - 1 );
+			$startDate.datepicker( 'option', 'maxDate', maxDate );
+		},
+	} );
 
-        $( '.woo-store-vacation-text-color-field, .woo-store-vacation-background-color-field' ).wpColorPicker();
+	$( '.woo-store-vacation-text-color-field, .woo-store-vacation-background-color-field' ).wpColorPicker();
 
-        // Adds a "Reset" control to the datepicker at the bottom
-        function cleanDatepicker() {
-           var old_fn = $.datepicker._updateDatepicker;
+	// Adds a "Reset" control to the datepicker at the bottom
+	function cleanDatepicker() {
+		const oldFn = $.datepicker._updateDatepicker;
 
-           $.datepicker._updateDatepicker = function( inst ) {
-              old_fn.call( this, inst );
-              var buttonPane = $( this ).datepicker( 'widget' ).find( '.ui-datepicker-buttonpane' );
-              $( "<button type='button' class='ui-datepicker-clean ui-state-default ui-priority-primary ui-corner-all'>Delete</button>" ).appendTo( buttonPane ).on( 'click', function( ev ) {
-                  $.datepicker._clearDate( inst.input );
-              } ) ;
-           }
-        }
-
+		$.datepicker._updateDatepicker = function ( inst ) {
+			oldFn.call( this, inst );
+			const buttonPane = $( this ).datepicker( 'widget' ).find( '.ui-datepicker-buttonpane' );
+			$(
+				"<button type='button' class='ui-datepicker-clean ui-state-default ui-priority-primary ui-corner-all'>Delete</button>"
+			)
+				.appendTo( buttonPane )
+				.on( 'click', function () {
+					$.datepicker._clearDate( inst.input );
+				} );
+		};
+	}
 } )( window.wp, jQuery );
